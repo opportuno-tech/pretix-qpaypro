fc700b9878099075f297112067c5c981af4b06f0
Alvaro Enrique Ruano <alvaro.ruano90@outlook.com>
2019-02-12 02:12:15 -0600

    Plugin basic information

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+from distutils.command.build import build
+
+from django.core import management
+from setuptools import setup, find_packages
+
+
+try:
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
+except:
+    long_description = ''
+
+
+class CustomBuild(build):
+    def run(self):
+        management.call_command('compilemessages', verbosity=1)
+        build.run(self)
+
+
+cmdclass = {
+    'build': CustomBuild
+}
+
+
+setup(
+    name='pretix-qpaypro',
+    version='1.0.0',
+    description='Integration for the QPayPro payment provider.',
+    long_description=long_description,
+    url='https://github.com/aruanoguate/pretix-qpaypro',
+    author='Alvaro Enrique Ruano',
+    author_email='alvaro.ruano90@outlook.com',
+    license='Apache Software License',
+
+    install_requires=[],
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    cmdclass=cmdclass,
+    entry_points="""
+[pretix.plugin]
+pretix_qpaypro=pretix_qpaypro:PretixPluginMeta
+""",
+)
