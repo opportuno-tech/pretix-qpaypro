6bee7459dd955b61d74f82e0dd0d6eb23dbc1759
Alvaro Enrique Ruano <alvaro.ruano90@outlook.com>
2019-03-14 04:32:19 -0600

    Disable all the code not used at this moment

--- pretix_qpaypro/payment.py
+++ pretix_qpaypro/payment.py
@@ -99,11 +99,11 @@ class QPayProMethod(QPayProSettingsHolder):
         return self.settings.get('_enabled', as_type=bool) and self.settings.get('method_{}'.format(self.method),
                                                                                  as_type=bool)
 
-    def payment_refund_supported(self, payment: OrderPayment) -> bool:
-        return self.refunds_allowed
+    # def payment_refund_supported(self, payment: OrderPayment) -> bool:
+    #     return self.refunds_allowed
 
-    def payment_partial_refund_supported(self, payment: OrderPayment) -> bool:
-        return self.refunds_allowed
+    # def payment_partial_refund_supported(self, payment: OrderPayment) -> bool:
+    #     return self.refunds_allowed
 
     def payment_prepare(self, request, payment):
         return self.checkout_prepare(request, None)
@@ -122,14 +122,14 @@ class QPayProMethod(QPayProSettingsHolder):
             request.session.get(key_prefix + 'cc_name', '') != ''
         )
 
-    @property
-    def request_headers(self):
-        headers = {}
-        if self.settings.connect_client_id and self.settings.access_token:
-            headers['Authorization'] = 'Bearer %s' % self.settings.access_token
-        else:
-            headers['Authorization'] = 'Bearer %s' % self.settings.api_key
-        return headers
+    # @property
+    # def request_headers(self):
+    #     headers = {}
+    #     if self.settings.connect_client_id and self.settings.access_token:
+    #         headers['Authorization'] = 'Bearer %s' % self.settings.access_token
+    #     else:
+    #         headers['Authorization'] = 'Bearer %s' % self.settings.api_key
+    #     return headers
 
     @property
     def payment_form_fields(self):
@@ -152,193 +152,193 @@ class QPayProMethod(QPayProSettingsHolder):
         }
         return template.render(ctx)
 
-    def payment_can_retry(self, payment):
-        return self._is_still_available(order=payment.order)
+    # def payment_can_retry(self, payment):
+    #     return self._is_still_available(order=payment.order)
 
-    def payment_pending_render(self, request, payment) -> str:
-        if payment.info:
-            payment_info = json.loads(payment.info)
-        else:
-            payment_info = None
-        template = get_template('pretix_qpaypro/pending.html')
-        ctx = {
-            'request': request,
-            'event': self.event,
-            'settings': self.settings,
-            'provider': self,
-            'order': payment.order,
-            'payment': payment,
-            'payment_info': payment_info,
-        }
-        return template.render(ctx)
+    # def payment_pending_render(self, request, payment) -> str:
+    #     if payment.info:
+    #         payment_info = json.loads(payment.info)
+    #     else:
+    #         payment_info = None
+    #     template = get_template('pretix_qpaypro/pending.html')
+    #     ctx = {
+    #         'request': request,
+    #         'event': self.event,
+    #         'settings': self.settings,
+    #         'provider': self,
+    #         'order': payment.order,
+    #         'payment': payment,
+    #         'payment_info': payment_info,
+    #     }
+    #     return template.render(ctx)
 
-    def payment_control_render(self, request, payment) -> str:
-        if payment.info:
-            payment_info = json.loads(payment.info)
-        else:
-            payment_info = None
-        template = get_template('pretix_qpaypro/control.html')
-        ctx = {
-            'request': request,
-            'event': self.event,
-            'settings': self.settings,
-            'payment_info': payment_info,
-            'payment': payment,
-            'method': self.method,
-            'provider': self,
-        }
-        return template.render(ctx)
+    # def payment_control_render(self, request, payment) -> str:
+    #     if payment.info:
+    #         payment_info = json.loads(payment.info)
+    #     else:
+    #         payment_info = None
+    #     template = get_template('pretix_qpaypro/control.html')
+    #     ctx = {
+    #         'request': request,
+    #         'event': self.event,
+    #         'settings': self.settings,
+    #         'payment_info': payment_info,
+    #         'payment': payment,
+    #         'method': self.method,
+    #         'provider': self,
+    #     }
+    #     return template.render(ctx)
 
-    def execute_refund(self, refund: OrderRefund):
-        payment = refund.payment.info_data.get('id')
-        body = {
-            'amount': {
-                'currency': self.event.currency,
-                'value': str(refund.amount)
-            },
-        }
-        if self.settings.connect_client_id and self.settings.access_token:
-            body['testmode'] = self.settings.endpoint == 'test'
-        try:
-            print(self.request_headers, body)
-            req = requests.post(
-                'https://api.qpaypro.com/v2/payments/{}/refunds'.format(payment),
-                json=body,
-                headers=self.request_headers
-            )
-            req.raise_for_status()
-            req.json()
-        except HTTPError:
-            logger.exception('QPayPro error: %s' % req.text)
-            try:
-                refund.info_data = req.json()
-            except:
-                refund.info_data = {
-                    'error': True,
-                    'detail': req.text
-                }
-            raise PaymentException(_('QPayPro reported an error: {}').format(refund.info_data.get('detail')))
-        else:
-            refund.done()
+    # def execute_refund(self, refund: OrderRefund):
+    #     payment = refund.payment.info_data.get('id')
+    #     body = {
+    #         'amount': {
+    #             'currency': self.event.currency,
+    #             'value': str(refund.amount)
+    #         },
+    #     }
+    #     if self.settings.connect_client_id and self.settings.access_token:
+    #         body['testmode'] = self.settings.endpoint == 'test'
+    #     try:
+    #         print(self.request_headers, body)
+    #         req = requests.post(
+    #             'https://api.qpaypro.com/v2/payments/{}/refunds'.format(payment),
+    #             json=body,
+    #             headers=self.request_headers
+    #         )
+    #         req.raise_for_status()
+    #         req.json()
+    #     except HTTPError:
+    #         logger.exception('QPayPro error: %s' % req.text)
+    #         try:
+    #             refund.info_data = req.json()
+    #         except:
+    #             refund.info_data = {
+    #                 'error': True,
+    #                 'detail': req.text
+    #             }
+    #         raise PaymentException(_('QPayPro reported an error: {}').format(refund.info_data.get('detail')))
+    #     else:
+    #         refund.done()
 
-    def get_locale(self, language):
-        pretix_to_qpaypro_locales = {
-            'en': 'en_US',
-            'nl': 'nl_NL',
-            'nl_BE': 'nl_BE',
-            'fr': 'fr_FR',
-            'de': 'de_DE',
-            'es': 'es_ES',
-            'ca': 'ca_ES',
-            'pt': 'pt_PT',
-            'it': 'it_IT',
-            'nb': 'nb_NO',
-            'sv': 'sv_SE',
-            'fi': 'fi_FI',
-            'da': 'da_DK',
-            'is': 'is_IS',
-            'hu': 'hu_HU',
-            'pl': 'pl_PL',
-            'lv': 'lv_LV',
-            'lt': 'lt_LT'
-        }
-        return pretix_to_qpaypro_locales.get(
-            language,
-            pretix_to_qpaypro_locales.get(
-                language.split('-')[0],
-                pretix_to_qpaypro_locales.get(
-                    language.split('_')[0],
-                    'en'
-                )
-            )
-        )
+    # def get_locale(self, language):
+    #     pretix_to_qpaypro_locales = {
+    #         'en': 'en_US',
+    #         'nl': 'nl_NL',
+    #         'nl_BE': 'nl_BE',
+    #         'fr': 'fr_FR',
+    #         'de': 'de_DE',
+    #         'es': 'es_ES',
+    #         'ca': 'ca_ES',
+    #         'pt': 'pt_PT',
+    #         'it': 'it_IT',
+    #         'nb': 'nb_NO',
+    #         'sv': 'sv_SE',
+    #         'fi': 'fi_FI',
+    #         'da': 'da_DK',
+    #         'is': 'is_IS',
+    #         'hu': 'hu_HU',
+    #         'pl': 'pl_PL',
+    #         'lv': 'lv_LV',
+    #         'lt': 'lt_LT'
+    #     }
+    #     return pretix_to_qpaypro_locales.get(
+    #         language,
+    #         pretix_to_qpaypro_locales.get(
+    #             language.split('-')[0],
+    #             pretix_to_qpaypro_locales.get(
+    #                 language.split('_')[0],
+    #                 'en'
+    #             )
+    #         )
+    #     )
 
-    def _get_payment_body(self, payment):
-        b = {
-            'amount': {
-                'currency': self.event.currency,
-                'value': str(payment.amount),
-            },
-            'description': 'Order {}-{}'.format(self.event.slug.upper(), payment.full_id),
-            'redirectUrl': build_absolute_uri(self.event, 'plugins:pretix_qpaypro:return', kwargs={
-                'order': payment.order.code,
-                'payment': payment.pk,
-                'hash': hashlib.sha1(payment.order.secret.lower().encode()).hexdigest(),
-            }),
-            'webhookUrl': build_absolute_uri(self.event, 'plugins:pretix_qpaypro:webhook', kwargs={
-                'payment': payment.pk
-            }),
-            'locale': self.get_locale(payment.order.locale),
-            'method': self.method,
-            'metadata': {
-                'organizer': self.event.organizer.slug,
-                'event': self.event.slug,
-                'order': payment.order.code,
-                'payment': payment.local_id,
-            }
-        }
-        if self.settings.connect_client_id and self.settings.access_token:
-            b['profileId'] = self.settings.connect_profile
-            b['testmode'] = self.settings.endpoint == 'test'
-        return b
+    # def _get_payment_body(self, payment):
+    #     b = {
+    #         'amount': {
+    #             'currency': self.event.currency,
+    #             'value': str(payment.amount),
+    #         },
+    #         'description': 'Order {}-{}'.format(self.event.slug.upper(), payment.full_id),
+    #         'redirectUrl': build_absolute_uri(self.event, 'plugins:pretix_qpaypro:return', kwargs={
+    #             'order': payment.order.code,
+    #             'payment': payment.pk,
+    #             'hash': hashlib.sha1(payment.order.secret.lower().encode()).hexdigest(),
+    #         }),
+    #         'webhookUrl': build_absolute_uri(self.event, 'plugins:pretix_qpaypro:webhook', kwargs={
+    #             'payment': payment.pk
+    #         }),
+    #         'locale': self.get_locale(payment.order.locale),
+    #         'method': self.method,
+    #         'metadata': {
+    #             'organizer': self.event.organizer.slug,
+    #             'event': self.event.slug,
+    #             'order': payment.order.code,
+    #             'payment': payment.local_id,
+    #         }
+    #     }
+    #     if self.settings.connect_client_id and self.settings.access_token:
+    #         b['profileId'] = self.settings.connect_profile
+    #         b['testmode'] = self.settings.endpoint == 'test'
+    #     return b
 
-    def execute_payment(self, request: HttpRequest, payment: OrderPayment):
-        try:
-            req = requests.post(
-                'https://api.qpaypro.com/v2/payments',
-                json=self._get_payment_body(payment),
-                headers=self.request_headers
-            )
-            req.raise_for_status()
-        except HTTPError:
-            logger.exception('QPayPro error: %s' % req.text)
-            try:
-                payment.info_data = req.json()
-            except:
-                payment.info_data = {
-                    'error': True,
-                    'detail': req.text
-                }
-            payment.state = OrderPayment.PAYMENT_STATE_FAILED
-            payment.save()
-            payment.order.log_action('pretix.event.order.payment.failed', {
-                'local_id': payment.local_id,
-                'provider': payment.provider,
-                'data': payment.info_data
-            })
-            raise PaymentException(_('We had trouble communicating with QPayPro. Please try again and get in touch '
-                                     'with us if this problem persists.'))
+    # def execute_payment(self, request: HttpRequest, payment: OrderPayment):
+    #     try:
+    #         req = requests.post(
+    #             'https://api.qpaypro.com/v2/payments',
+    #             json=self._get_payment_body(payment),
+    #             headers=self.request_headers
+    #         )
+    #         req.raise_for_status()
+    #     except HTTPError:
+    #         logger.exception('QPayPro error: %s' % req.text)
+    #         try:
+    #             payment.info_data = req.json()
+    #         except:
+    #             payment.info_data = {
+    #                 'error': True,
+    #                 'detail': req.text
+    #             }
+    #         payment.state = OrderPayment.PAYMENT_STATE_FAILED
+    #         payment.save()
+    #         payment.order.log_action('pretix.event.order.payment.failed', {
+    #             'local_id': payment.local_id,
+    #             'provider': payment.provider,
+    #             'data': payment.info_data
+    #         })
+    #         raise PaymentException(_('We had trouble communicating with QPayPro. Please try again and get in touch '
+    #                                  'with us if this problem persists.'))
 
-        data = req.json()
-        payment.info = json.dumps(data)
-        payment.state = OrderPayment.PAYMENT_STATE_CREATED
-        payment.save()
-        request.session['payment_qpaypro_order_secret'] = payment.order.secret
-        return self.redirect(request, data.get('_links').get('checkout').get('href'))
+    #     data = req.json()
+    #     payment.info = json.dumps(data)
+    #     payment.state = OrderPayment.PAYMENT_STATE_CREATED
+    #     payment.save()
+    #     request.session['payment_qpaypro_order_secret'] = payment.order.secret
+    #     return self.redirect(request, data.get('_links').get('checkout').get('href'))
 
-    def redirect(self, request, url):
-        if request.session.get('iframe_session', False):
-            signer = signing.Signer(salt='safe-redirect')
-            return (
-                    build_absolute_uri(request.event, 'plugins:pretix_qpaypro:redirect') + '?url=' +
-                    urllib.parse.quote(signer.sign(url))
-            )
-        else:
-            return str(url)
+    # def redirect(self, request, url):
+    #     if request.session.get('iframe_session', False):
+    #         signer = signing.Signer(salt='safe-redirect')
+    #         return (
+    #                 build_absolute_uri(request.event, 'plugins:pretix_qpaypro:redirect') + '?url=' +
+    #                 urllib.parse.quote(signer.sign(url))
+    #         )
+    #     else:
+    #         return str(url)
 
-    def shred_payment_info(self, obj: OrderPayment):
-        if not obj.info:
-            return
-        d = json.loads(obj.info)
-        if 'details' in d:
-            d['details'] = {
-                k: '█' for k in d['details'].keys()
-                if k not in ('bitcoinAmount', )
-            }
+    # def shred_payment_info(self, obj: OrderPayment):
+    #     if not obj.info:
+    #         return
+    #     d = json.loads(obj.info)
+    #     if 'details' in d:
+    #         d['details'] = {
+    #             k: '█' for k in d['details'].keys()
+    #             if k not in ('bitcoinAmount', )
+    #         }
 
-        d['_shredded'] = True
-        obj.info = json.dumps(d)
-        obj.save(update_fields=['info'])
+    #     d['_shredded'] = True
+    #     obj.info = json.dumps(d)
+    #     obj.save(update_fields=['info'])
 
 
 class QPayProCC(QPayProMethod):
