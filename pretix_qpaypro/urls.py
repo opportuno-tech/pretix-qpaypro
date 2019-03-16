affccafa78b0a994ac76dd29ab0e9a9766dbfaba
Alvaro Enrique Ruano <alvaro.ruano90@outlook.com>
2019-03-15 23:24:02 -0600

    Disable all the code not used at this moment

--- pretix_qpaypro/urls.py
+++ pretix_qpaypro/urls.py
@@ -1,18 +1,18 @@
 from django.conf.urls import include, url
 from pretix.multidomain import event_url
 
-from .views import (
-    ReturnView, WebhookView, redirect_view, forward_onlinemetrix, onlinemetrix_internal_url,
-)
+# from .views import (
+#     ReturnView, WebhookView, redirect_view, forward_onlinemetrix, onlinemetrix_internal_url,
+# )
 
 event_patterns = [
-    url(r'^QPayPro/', include([
-        event_url(r'^webhook/(?P<payment>[0-9]+)/$', WebhookView.as_view(), name='webhook', require_live=False),
-        url(r'^redirect/$', redirect_view, name='redirect'),
-        url(r'^return/(?P<order>[^/]+)/(?P<hash>[^/]+)/(?P<payment>[0-9]+)/$', ReturnView.as_view(), name='return'),
+    url(r'^qpaypro/', include([
+        # event_url(r'^webhook/(?P<payment>[0-9]+)/$', WebhookView.as_view(), name='webhook', require_live=False),
+        # url(r'^redirect/$', redirect_view, name='redirect'),
+        # url(r'^return/(?P<order>[^/]+)/(?P<hash>[^/]+)/(?P<payment>[0-9]+)/$', ReturnView.as_view(), name='return'),
     ])),
 ]
 
-urlpatterns = [
-    url(r'^' + onlinemetrix_internal_url[1:] + r'(?P<all>.*)$', forward_onlinemetrix, name='onlinemetrix'),
-]
+# urlpatterns = [
+#     url(r'^' + onlinemetrix_internal_url[1:] + r'(?P<all>.*)$', forward_onlinemetrix, name='onlinemetrix'),
+# ]
